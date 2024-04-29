4b2dfc8cd99efda6cc001aff1a79e42554d39201
ovpn-dev <ovpn05@gmail.com>
2024-04-29 07:56:31 +0100

    www

--- src/components/Dashboard/upgradeModal.jsx
+++ src/components/Dashboard/upgradeModal.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import React, { useState, useEffect } from "react";
 import {
   Button,
   Modal,
@@ -15,14 +15,34 @@ import {
   Box,
   Image,
   Flex,
+  Heading,
+  ButtonGroup,
+  Stack,
+  Tabs,
+  TabList,
+  TabPanels,
+  Tab,
+  TabPanel,
+  Table,
+  Tbody,
+  Tr,
+  Td,
+  TableContainer,
+  Link,
+  Icon,
+  NumberInput,
+  NumberInputField,
 } from "@chakra-ui/react";
+import Rec9 from "../../images/Rectangle9.png";
 import { BiSolidPlug } from "react-icons/bi";
 import { IoIosFlash } from "react-icons/io";
 import { IoIosAddCircleOutline } from "react-icons/io";
-import { Link } from "@chakra-ui/next-js";
+import { SiBitcoincash } from "react-icons/si";
+import { ArrowForwardIcon, InfoOutlineIcon } from "@chakra-ui/icons";
+// import { Link } from "@chakra-ui/next-js";
 import CModal from "./createModal";
 
-export default function UpgradeModal() {
+export default function UpgradeModal({ user }) {
   const { isOpen, onOpen, onClose } = useDisclosure();
   const [showList, setShowList] = useState(false); // Initialize showForm state
   const [showCard, setShowCard] = useState(false); // Add state for controlling card visibility
@@ -35,6 +55,22 @@ export default function UpgradeModal() {
     setShowList(!showList); // Toggle the showList state
     setShowCard(false); // Reset the showCard state
   };
+  const [power, setpower] = useState("");
+  const [payout, setPayout] = useState(0);
+
+  useEffect(() => {
+    console.log(power);
+    const payout = (power * 24 * 300) / 100;
+    setPayout(payout);
+  }, [power]);
+
+  console.log(user);
+
+  const [miner, setMiner] = useState(null);
+  const [balance, setBalance] = useState(0);
+
+  const [value, setValue] = React.useState(0);
+  const handleChange = (value) => setpower(value);
 
   return (
     <>
@@ -57,13 +93,14 @@ export default function UpgradeModal() {
         >
           <ModalHeader>Upgrade your miner</ModalHeader>
           <ModalCloseButton />
-          <ModalBody>
-            <Text>
+          <ModalBody gap={10}>
+            <Text mb={5}>
               Choose a pack or customize an upgrade yourself to get more rewards
             </Text>
             <Button
               leftIcon={<IoIosAddCircleOutline />}
               onClick={handleAddMinerClick}
+              mb={5}
             >
               Add Miner
             </Button>
@@ -124,7 +161,156 @@ export default function UpgradeModal() {
                 </Flex>
               </Box>
             )}
-            {showCard && <CModal />}{" "}
+            {showCard && (
+              <Stack
+                bg={useColorModeValue("#fff", "#10062D")}
+                color={useColorModeValue("#10062D", "#fff")}
+                border="2px solid"
+                borderColor={useColorModeValue("#EDE8FC", "#301287")}
+              >
+                <Heading size={"sm"}>Computing power</Heading>
+                <ButtonGroup gap="4" variant={"outline"} mb={4}>
+                  <Button
+                    value={1}
+                    onClick={(e) => setpower(e.target.value)}
+                    color={"#00D87D"}
+                    border="1px solid #301287"
+                    fontSize={["12px", "initial"]}
+                  >
+                    1 TH
+                  </Button>
+                  <Button
+                    value={10}
+                    onClick={(e) => setpower(e.target.value)}
+                    color={"#00D87D"}
+                    border="1px solid #301287"
+                    fontSize={["12px", "initial"]}
+                  >
+                    10 TH
+                  </Button>
+                  <Button
+                    value={100}
+                    onClick={(e) => setpower(e.target.value)}
+                    color={"#00D87D"}
+                    border="1px solid #301287"
+                    fontSize={["12px", "initial"]}
+                  >
+                    100 TH
+                  </Button>
+                  <NumberInput
+                    min={1}
+                    value={power}
+                    onChange={handleChange}
+                    max={1000} // Set the maximum value for the NumberInput
+                  >
+                    <NumberInputField placeholder={"Value"} p={"5px"} />
+                  </NumberInput>
+                </ButtonGroup>
+                <Heading size={"sm"}>Rewards Calculation</Heading>
+                <Tabs variant="enclosed" textColor="white">
+                  <TabList gap={1} mb={2} border={"none"}>
+                    <Tab
+                      bg="#3b49df"
+                      rounded={"lg"}
+                      border={"none"}
+                      textColor="white"
+                      w={100}
+                    >
+                      Annually
+                    </Tab>
+                    {/* <Tab
+                    bg="#3b49df"
+                    rounded={"lg"}
+                    border={"none"}
+                    textColor="white"
+                    w={100}
+                  >
+                    Daily
+                  </Tab> */}
+                  </TabList>
+                  <TabPanels>
+                    <TabPanel
+                      backgroundImage={`url(${Rec9.src})`}
+                      backgroundSize="cover"
+                      backgroundPosition="center"
+                      rounded={"lg"}
+                    >
+                      <TableContainer p={2} borderRadius={"lg"}>
+                        <Table variant="simple">
+                          <Tbody>
+                            <Tr>
+                              <Td fontSize="xs">POOL PAYOUT</Td>
+                              <Td isNumeric align="center">
+                                {payout}
+                                <Icon
+                                  boxSize={3}
+                                  as={SiBitcoincash}
+                                  color={"yellow.50"}
+                                />
+                              </Td>
+                            </Tr>
+                            <Tr>
+                              <Td fontSize="xs">
+                                <Flex
+                                  align={"center"}
+                                  justify={"space-between"}
+                                >
+                                  <Text>NET REWARD</Text> <InfoOutlineIcon />
+                                </Flex>
+                              </Td>
+                              <Td isNumeric>${payout}</Td>
+                            </Tr>
+                          </Tbody>
+                        </Table>
+                      </TableContainer>
+
+                      <Stack p={2} fontSize="xs">
+                        <Text>Reward history</Text>
+                        {/* <Flex
+                        p={3}
+                        bg={"gray.400"}
+                        rounded="2xl"
+                        align={"center"}
+                        justify={"space-between"}
+                        gap={3}
+                      >
+                        <InfoOutlineIcon boxSize={4} />
+                        <Text fontSize={"9px"} color={"white"}>
+                          Bitcoin halving is expected around 21 April. Make sure
+                          you adjust your investment strategy.
+                        </Text>
+                      </Flex> */}
+                      </Stack>
+
+                      <Flex p={2} align={"center"} justify="space-between">
+                        <Link>
+                          {" "}
+                          <Text>Miner traits</Text>
+                          <ArrowForwardIcon />
+                        </Link>
+                      </Flex>
+                      <Stack border={"2px solid #301287"} rounded={"lg"} p={2}>
+                        <Flex align={"center"} justify={"space-between"}>
+                          <Text>Price per TH</Text>
+                          <Text>$24</Text>
+                        </Flex>
+                        <Flex align={"center"} justify={"space-between"}>
+                          <Text>Historical ROI</Text>
+                          <Text>${payout}</Text>
+                        </Flex>
+                        <Flex align={"center"} justify={"space-between"}>
+                          <Text>Total</Text>
+                          <Text>${power * 24}</Text>
+                        </Flex>
+                      </Stack>
+                    </TabPanel>
+                    <TabPanel>
+                      <p>two!</p>
+                    </TabPanel>
+                  </TabPanels>
+                </Tabs>
+              </Stack>
+            )}
             {/* Render the card component when showCard is true */}
           </ModalBody>
 
